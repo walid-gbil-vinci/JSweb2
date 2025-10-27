90e5867efb8fff1a1ce3cb5e54bd507b712a2ec5
walid.gbil <walid.gbil@student.vinci.be>
2025-10-27 16:39:14 +0100

    new: ex1.8

---
+++ exercises/1.8/types.ts
@@ -0,0 +1,12 @@
+interface Film {
+  id: number;
+  title: string;
+  director: string;
+  duration: number;
+  budget?: number;
+  description?: string;
+  imageUrl?: string;
+}
+
+type NewFilm = Omit<Film, "id">;
+export type { Film, NewFilm };
