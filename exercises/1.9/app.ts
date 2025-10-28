cfa18605be4e6ebbdad9c0faeabf6f980e7cb220
walid.gbil <walid.gbil@student.vinci.be>
2025-10-28 17:52:37 +0100

    new: ex1.9

---
+++ exercises/1.9/app.ts
@@ -0,0 +1,26 @@
+import express from "express";
+
+import pizzaRouter from "./routes/films";
+import textRouter from "./routes/texts";
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+/* Middleware to count the number of GET requests */
+let requestCount = 0;
+app.use((req, _res, next) => {
+  if (req.method === "GET") {
+    requestCount++;
+    console.log(`GET counter : ${requestCount}`);
+  }
+  next();
+});
+
+
+app.use("/films", pizzaRouter);
+app.use("/texts", textRouter);
+
+export default app;
