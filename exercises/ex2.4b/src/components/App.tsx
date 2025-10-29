5172eb8ec3fbd7f98d83dcfa8fa80042cc18cf3a
walid.gbil <walid.gbil@student.vinci.be>
2025-10-29 18:33:18 +0100

    new: ex2.4b

---
+++ exercises/ex2.4b/src/components/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import type { User } from "./type";
+import UserCard from "./UserCard";
+
+const users: User[] = [
+  { name: "John Doe", age: 25, isOnline: true },
+  { name: "Jane Doe", age: 22, isOnline: false },
+  { name: "Foo Bar", age: 30, isOnline: true },
+];
+
+const App = () => (
+  <>
+    <h1>Users</h1>
+    {users.map((user, index) => (
+      <UserCard key={index} user={user} />
+    ))}
+  </>
+);
+
+export default App;
