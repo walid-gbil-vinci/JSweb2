f2738634d460587d793aa3795bb06c1ffc497b12
walid.gbil <walid.gbil@student.vinci.be>
2025-11-25 17:02:38 +0100

    new: ex2.6

---
+++ exercises/2.6/src/components/Footer.tsx
@@ -0,0 +1,15 @@
+interface FooterProps {
+  urlLogo: string;
+  children: React.ReactNode;
+}
+
+const Footer = (props: FooterProps) => {
+  return (
+    <footer className="footer">
+      <div>{props.children}</div>
+      <img src={props.urlLogo} alt="logo" className="logo" />
+    </footer>
+  );
+};
+
+export default Footer;
